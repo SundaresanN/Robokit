597e9c4624ca04f8417b3d8ed25064334a5464c1
SundaresanN <31757231+SundaresanN@users.noreply.github.com>
2018-11-30 14:40:24 +0530

    Module for setting Wifi Access point

--- WifiBot_Android/uWifi_AP.py
+++ WifiBot_Android/uWifi_AP.py
@@ -1,12 +1,17 @@
 
 
 import network
+
 def ap_start():
+  #configuring access point
   ap = network.WLAN(network.AP_IF)
+  #Resetting AP
   ap.active(False)
   ap.active(True)
+  #configuring ssid and password
   ap.config(essid = 'RC-CAR')
   ap.config(authmode =3 ,password = '123456789')
+  #printing ip address
   print('network config:',ap.ifconfig())
 
 
