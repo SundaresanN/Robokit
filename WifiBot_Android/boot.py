89ef99c3fae7eb250f13f73992063d572c2dfc57
SundaresanN <31757231+SundaresanN@users.noreply.github.com>
2018-11-30 14:23:21 +0530

    Update boot.py
    Boot File - The file that runs on start up.

--- WifiBot_Android/boot.py
+++ WifiBot_Android/boot.py
@@ -1,26 +1,16 @@
 
-
-
-
-
-
-
-
-
-
 # This file is executed on every boot (including wake-boot from deepsleep)
 
+#Turn off debugging
 import esp
-
 esp.osdebug(None)
 
+#Garbage collector
 import gc
-
 gc.collect()
 
-
+#Setting Access Point
 import uWifi_AP as Wifi_AP
-
 Wifi_AP.ap_start()
 
 
