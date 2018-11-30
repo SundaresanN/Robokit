64c34445e9f31e1638d0b161660259a0b7268f4e
SundaresanN <31757231+SundaresanN@users.noreply.github.com>
2018-11-30 14:32:19 +0530

    Server Module

--- WifiBot_Android/uBOT.py
+++ WifiBot_Android/uBOT.py
@@ -1,5 +1,4 @@
 
-
 try:
   import usocket as socket
 except:
@@ -10,30 +9,33 @@ import network
 import uRover as Motor
 
 def Start_Bot():
-
+  #initializes Socket
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   s.bind(('', 80))
   s.listen(2)
-
+  
+  #object for stepper class
   M = Motor.Stepper()
 
   while True:
+    #connection established
     conn, addr = s.accept()
     print('Got a connection from %s' % str(addr))
-
+    
+    #receives requests
     request = conn.recv(1024)
     request = str(request)
     print('Content = %s' % request)
-  
+    
+    #parse request to identify command
     forward = request.find('/forward')
     backward = request.find('/backward')
-
     left = request.find('/left')
     right = request.find('/right')
     stop = request.find('/stop')
   
+    #controls bot based on request
     if forward != -1:
-
       M.Move_Forward()
     
     elif backward != -1:
@@ -48,6 +50,7 @@ def Start_Bot():
     elif stop != -1:
       M.STOP()
     
+    #connection closed
     conn.close()
 
 
